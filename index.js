7683d0e6314cce360313fc5a0863c4806722c14d
Albright <albright.muchori@student.moringaschool.com>
2023-06-14 01:04:52 +0300

    arithemetics

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
-// Write your code here
+// Write your code cd
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+
+console.log(multiply); // Output: 62
+
+var random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random); // Output: A random integer greater than 0
+
+var num3 = 14;
+var num4 = 5;
+var mod = num3 % num4;
+
+console.log(mod); // Output: 4
+
+var numbers = [5, 10, 20, 15];
+var max = Math.max(...numbers);
+
+console.log(max); // Output: 20
